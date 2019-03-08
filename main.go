d4807440d504bb50dbcdd989bc5186ce78f216a8
EvanPan <EvanPan@boyaa.com>
2019-03-08 12:28:04 +0800

    fix: 接口改用中划线

--- main.go
+++ main.go
@@ -1,29 +1,19 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sirupsen/logrus"
-	"github.com/dy-platform/user-api-passport/router"
 	"github.com/dy-gopkg/kit/web"
+	"github.com/dy-platform/user-api-passport/router"
 )
 
 func main() {
 	// Create service
-	fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaaa")
 	web.Init()
-	fmt.Println("bbbbbbbbbbbbbbbbbbb")
 
 	r := router.Init()
-
-	fmt.Println("ccccccccccccccccccccc")
 
 	// Register Handler
 	web.DefaultService.Handle("/", r)
 
-	fmt.Println("ddddddddddddddddddd")
-
-
-	logrus.Debugf("aaaaaaaaaaaaaaaaaaaaa")
 	// Run server
 	web.Run()
 }
--- router/router.go
+++ router/router.go
@@ -11,7 +11,7 @@ func Init() *gin.Engine {
 
 	r := gin.Default()
 
-	r.POST("/wechat_signin", WeChatSignIn)
+	r.POST("/wechat-signin", WeChatSignIn)
 
 
 	return  r
