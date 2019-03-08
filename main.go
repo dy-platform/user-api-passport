9b32be069f0a1df09aa7934432d08d145a48dee3
EvanPan <EvanPan@boyaa.com>
2019-03-08 10:20:55 +0800

    tst

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/sirupsen/logrus"
 	"github.com/dy-platform/user-api-passport/router"
 	"github.com/dy-gopkg/kit/web"
@@ -8,11 +9,19 @@ import (
 
 func main() {
 	// Create service
+	fmt.Println("aaaaaaaaaaaaaaaaaaaaaaaaa")
 	web.Init()
+	fmt.Println("bbbbbbbbbbbbbbbbbbb")
 
 	r := router.Init()
+
+	fmt.Println("ccccccccccccccccccccc")
+
 	// Register Handler
 	web.DefaultService.Handle("/", r)
+
+	fmt.Println("ddddddddddddddddddd")
+
 
 	logrus.Debugf("aaaaaaaaaaaaaaaaaaaaa")
 	// Run server
